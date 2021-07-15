66dd6b632d2410f4354769f035f6492cbeb00db1
kpV2 <86794853+kpV2@users.noreply.github.com>
2021-07-15 13:04:25 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,11 +48,8 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Group 1</b> : <b><i><a href="https://t.me/bookflowers2">Group</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Group 2</b> : <i><a href="https://t.me/Educationworldd">Click Me</a></i>
 """
